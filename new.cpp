20db8160f9a44b76a572f52a565acdc32980445b
Varsha Rohidekar <98642472+VarshaRohidekar@users.noreply.github.com>
2024-03-05 21:15:22 +0530

    Create new.cpp

---
+++ new.cpp
@@ -0,0 +1,10 @@
+//File: new.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "This is the new file\n";
+  return 0;
+}
