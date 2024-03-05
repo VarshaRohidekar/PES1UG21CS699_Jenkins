76b3b8e52b9b8dd4f3c33943e2da7d6b1110d604
Varsha Rohidekar <varsharohidekar@192.168.1.2>
2024-03-05 20:37:58 +0530

    Changes were made

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,5 +7,6 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   cout << "Checking automatic job execution\n";
+  cout << "Testing\n";
   return 0;
 }
